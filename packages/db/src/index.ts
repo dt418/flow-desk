16cfae45b3e10bb6d7c41dc02aa5ed5ebcc768b8
thanhd <danhthanh418@gmail.com>
2026-06-24 22:59:01 +0700

    feat(db): create packages/db with Prisma 7 factory pattern

---
+++ packages/db/src/index.ts
@@ -0,0 +1,3 @@
+export { createPrismaClient, getPrisma } from './client';
+export type { PrismaClient } from '../generated/client';
+export { softDeleteExtension, SOFT_DELETE_MODEL_NAMES } from './prisma-extension';
